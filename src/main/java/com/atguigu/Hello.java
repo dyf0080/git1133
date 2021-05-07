663b170226952f3bdf3317efd2de244c3f966734
duyufan <dyf0080@163.com>
2021-05-07 20:14:35 +0800

    123 commit

---
+++ src/main/java/com/atguigu/Hello.java
@@ -0,0 +1,11 @@
+package com.atguigu;
+
+/**
+ * @author duyufan
+ * @create 2021-05-07 20:10
+ */
+public class Hello {
+    public static void main(String[] args){
+        System.out.println("123");
+    }
+}
